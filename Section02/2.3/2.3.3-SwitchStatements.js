1a5c39b0cb9c1913e16cfedb5d07c64a4a6bfcb0
Bri'Anna Tolliver <bri39annat@gmail.com>
2022-11-04 20:18:09 -0500

    Worked on Switch statement exercise

--- Section02/2.3/2.3.3-SwitchStatements.js
+++ Section02/2.3/2.3.3-SwitchStatements.js
@@ -1,17 +1,22 @@
 // Correct these statements so they make sense
-// Sunday = 0, Monday = 1, ... Saturday = 6
-// re-assign day to any number from 0 - 7 to test the result
-let day = 0;
+// Sunday = 0, Monday = 1, T=2,W=3Th=4,F=5... Saturday = 6
+// re-assign day to any number from 0 - 6 to test the result
+let day = 1;
 let dayName = `Today`;
 
 // Complete this switch case statement to produce the correct results please.
 
 switch (day) {
-  case 3:
-    dayName = `Today is Wednesday`;
-    break;
   case 1:
     dayName = `Today is Monday`;
+    break;
+
+  case 2:
+    console.log(`Today is Tuesday`);
+    break;
+
+  case 3:
+    dayName = `Today is Wednesday`;
     break;
 
   // Note the code blocks in the next 2 cases: Why?
@@ -20,23 +25,21 @@ switch (day) {
     console.log(message);
     break;
   }
+
   case 5: {
     const message = `Tomorrow it's the weekend!`;
     console.log(message);
     break;
   }
-  default:
-    console.log(`It is not any day I guess`);
-    break;
-  case 2:
-    console.log(`Today is Tuesday`);
-    break;
   // write a case here for the weekend (how many days in the weekend?)
   case 6:
   case 0:
     console.log(`It's the Weekend !!`);
+    break;
 
+  default:
+    console.log(`It is not any day I guess`);
 }
-if (dayName !== 'Today'){
-console.log(dayName);
+if (dayName !== "Today") {
+  console.log(dayName);
 }
